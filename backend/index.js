5a36d743faba7e71430c1602ccf129667f6db50e
Hector Cryo <sonic200887@gmail.com>
2024-07-21 14:45:29 +0300

    slightly adjusted vite.config and package.json for backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3400;
+const port = process.env.PORT || 5432;
 const https = require('https');
 const fs = require('fs');
 const path = require('path');

3b9d8dd1e8d034dfebbf08a8ab1f24938d1f4c50
Hector Cryo <sonic200887@gmail.com>
2024-07-21 14:45:29 +0300

    slightly adjusted vite.config and package.json for backend

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(
 );
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.BACKEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 
